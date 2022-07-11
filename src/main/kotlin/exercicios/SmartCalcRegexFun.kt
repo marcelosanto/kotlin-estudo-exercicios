bb7befdaf9c81e8d00cacd5eba47b2cd7efff28d
Marcelo Santos <rocha.mer21@gmail.com>
2022-07-11 16:09:54 -0300

    fix: bug dos espaçoes

--- src/main/kotlin/exercicios/SmartCalcRegexFun.kt
+++ src/main/kotlin/exercicios/SmartCalcRegexFun.kt
@@ -2,30 +2,60 @@ package src.main.kotlin.exercicios
 
 
 fun main() {
-    val cont = "3 + (2 + 1"
-    val conta = "2 - 2 + 3"
+    val letras = Regex("[a-zA-Z]+")
+    val listDeVariaveis = mutableMapOf<String, Int>()
 
-    //val withParentheses = Regex("(^\\d+\\s*(\\+|-|\\*|/)\\s*\\d+)+")
-
-    //println(cont.matches(withParentheses))
-    //println(withParentheses.find(cont)?.value)
-    println(calculatorFunction("3 + 8 * ((4 + 3) * 2 + 1) - 6 / (2 + 1)"))
+    while (true) {
+        val str = readln().trimEnd()
+        if (str.isBlank()) {
+            continue
+        } else if (letras.find(str)?.value == null) {
+            println(calculatorFunction(str))
+        } else if (letras.find(str)?.value != null && "/[a-zA-Z]".toRegex().find(str)?.value == null) {
+            if ("=".toRegex().find(str)?.value != null) {
+                val (x, y) = str.replace("\\s*".toRegex(), "").split("=")
+                listDeVariaveis[x] = y.toInt()
+                println(listDeVariaveis)
+            } else {
+                if (listDeVariaveis[str] != null) println(listDeVariaveis[str])
+                else println("Unknown variable")
+            }
+        } else if ("/[a-zA-Z]".toRegex().find(str)?.value != null) break
+    }
 
 }
 
+fun formatMultipleSigns(calculatorNumbers: String): String {
+    val multipleSigns = Regex("[+|-]{2,}")
+
+    var tempNumbersMutiplySings = calculatorNumbers
+
+    if (multipleSigns.find(tempNumbersMutiplySings)?.value != null) {
+        val temp = multipleSigns.find(tempNumbersMutiplySings)?.value
+
+        if (temp!!.matches("-+".toRegex())) {
+            if (temp!!.length % 2 == 0) {
+                tempNumbersMutiplySings = tempNumbersMutiplySings.replace(temp, "+")
+            } else tempNumbersMutiplySings = tempNumbersMutiplySings.replace(temp, "-")
+        } else tempNumbersMutiplySings = tempNumbersMutiplySings.replace(temp, "+")
+    }
+
+    return tempNumbersMutiplySings
+}
+
 fun calculatorFunction(str: String): String {
-    val noParentheses = Regex("(^\\d+\\s*(\\+|-|\\*|/)\\s*\\d+)+")
-    val withParentheses = Regex("(\\(\\s*\\d+\\s*(\\+|-|\\*|/)\\s*\\d+\\s*(\\+|-|\\*|/)?\\s*\\d*\\s*\\))")
+    val noParentheses = Regex("(^-?\\d+\\s*(\\+|-|\\*|/|\\^)\\s*\\d+)+")
+    val withParentheses = Regex("(\\(\\s*\\d+\\s*(\\+|-|\\*|/|\\^)\\s*\\d+\\s*(\\+|-|\\*|/|\\^)?\\s*\\d*\\s*\\))")
+    val multipleSigns = Regex("[+|-]{2,}")
     var result = str
 
     while (true) {
-        if (withParentheses.find(result)?.value != null) {
+        if (multipleSigns.find(result)?.value != null) {
+            result = formatMultipleSigns(result)
+        } else if (withParentheses.find(result)?.value != null) {
             result = sumWithParentheses(result, withParentheses)
         } else if (noParentheses.find(result)?.value != null) {
             result = calcNumbers(result)
-        } else if (withParentheses.find(result)?.value == null && noParentheses.find(result)?.value == null) {
-            result = "Invalid expression"
-            break
         } else {
             break
         }
@@ -35,47 +65,64 @@ fun calculatorFunction(str: String): String {
 }
 
 fun calcNumbers(numbers: String): String {
-    var calcTemp = 0
-    var result = ""
+    var calcTemp: Int
+    var result: String
 
-    val regexMulti = Regex("\\d+\\s*\\*\\s*\\d+")
-    val regexDiv = Regex("\\d+\\s*/\\s*\\d+")
-    val regexSub = Regex("\\d+\\s*-\\s*\\d+")
-    val regexSum = Regex("\\d+\\s*\\+\\s*\\d+")
+    val regexMulti = Regex("-?\\d+\\s*\\*\\s*\\d+")
+    val regexDiv = Regex("-?\\d+\\s*/\\s*\\d+")
+    val regexElev = Regex("-?\\d+\\s*\\^\\s*\\d+")
 
     val calculator: (Int, Int, String) -> Int = function()
 
-    if (regexMulti.find(numbers)?.value != null) {
-        val teste = regexMulti.find(numbers)?.value?.split(" ")?.toMutableList()
+    if (regexElev.find(numbers)?.value != null) {
+        val (x, y) = regexElev.find(numbers)?.value?.replace("\\s*".toRegex(), "")?.split("^")!!.map { it.toInt() }
+        val temp = regexElev.find(numbers)?.value
+        result = numbers.replace(temp!!, elevado(x, y).toString())
+
+    } else if (regexMulti.find(numbers)?.value != null) {
+        val teste = regexMulti.find(numbers)?.value?.replace("\\s*".toRegex(), "")?.replace("*", " * ")?.split(" ")
+            ?.toMutableList()
+
         val temp = regexMulti.find(numbers)?.value
+
         calcTemp = teste?.let { calculator(teste[0].toInt(), teste[2].toInt(), teste[1]) }!!
         result = numbers.replace(temp!!, calcTemp.toString())
 
+
     } else if (regexDiv.find(numbers)?.value != null) {
-        val teste = regexDiv.find(numbers)?.value?.split(" ")?.toMutableList()
+        val teste = regexDiv.find(numbers)?.value?.replace("\\s*".toRegex(), "")?.replace("/", " / ")?.split(" ")
+            ?.toMutableList()
         val temp = regexDiv.find(numbers)?.value
+
         calcTemp = teste?.let { calculator(teste[0].toInt(), teste[2].toInt(), teste[1]) }!!
         result = numbers.replace(temp!!, calcTemp.toString())
-    } else if (regexSub.find(numbers)?.value != null) {
-        val teste = regexSub.find(numbers)?.value?.split(" ")?.toMutableList()
-        val temp = regexSub.find(numbers)?.value
-        calcTemp = teste?.let { calculator(teste[0].toInt(), teste[2].toInt(), teste[1]) }!!
-        result = numbers.replace(temp!!, calcTemp.toString())
-    } else if (regexSum.find(numbers)?.value != null) {
-        val teste = regexSum.find(numbers)?.value?.split(" ")?.toMutableList()
-        val temp = regexSum.find(numbers)?.value
-        calcTemp = teste?.let { calculator(teste[0].toInt(), teste[2].toInt(), teste[1]) }!!
-        result = numbers.replace(temp!!, calcTemp.toString())
+    } else {
+        val listInt = mutableListOf<Int>()
+
+        numbers.replace("\\s+".toRegex(), "").replace("-", ",-").replace("+", ",+").replace("^,".toRegex(), "")
+            .split(",").map { listInt.add(it.toInt()) }
+
+        result = listInt.sum().toString()
     }
 
     return result
 }
 
+fun elevado(x: Int, y: Int): Int {
+    //valor x elevado
+    var h = 1
+
+    for (i in 1..y) {
+        h *= x
+    }
+
+    return h
+}
+
 private fun function(): (Int, Int, String) -> Int {
     val calculator: (Int, Int, String) -> Int = { number01: Int, number02: Int, operation: String ->
+
         when (operation) {
-            "+" -> number01 + number02
-            "-" -> number01 - number02
             "*" -> number01 * number02
             "/" -> number01 / number02
             else -> 0
