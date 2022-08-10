f9af4c2785731600c3b3b8733745ed61e16eeb91
Marcelo Santos <rocha.mer21@gmail.com>
2022-08-09 21:34:48 -0300

    estudo sobre enum

---
+++ src/main/kotlin/fundamentos/enum.kt
@@ -0,0 +1,51 @@
+package src.main.kotlin.fundamentos
+
+fun main() {
+    Repository.startFetch()
+    getResul(Repository.getCurrentState())
+    Repository.finishedFetch()
+    getResul(Repository.getCurrentState())
+    Repository.error()
+    getResul(Repository.getCurrentState())
+}
+
+fun getResul(result: Result) {
+    return when (result) {
+        Result.SUCCESS -> println("Success!")
+        Result.FAILURE -> println("Failure!")
+        Result.ERROR -> println("Error!")
+        Result.IDLE -> println("Idle!")
+        Result.LOADING -> println("Loading!")
+    }
+}
+
+object Repository {
+    private var loadState: Result = Result.IDLE
+    private var dataFetched: String? = null
+
+    fun startFetch() {
+        loadState = Result.LOADING
+        dataFetched = "data"
+    }
+
+    fun finishedFetch() {
+        loadState = Result.SUCCESS
+        dataFetched = null
+    }
+
+    fun error() {
+        loadState = Result.ERROR
+    }
+
+    fun getCurrentState(): Result {
+        return loadState
+    }
+}
+
+enum class Result {
+    SUCCESS,
+    FAILURE,
+    ERROR,
+    IDLE,
+    LOADING
+}
